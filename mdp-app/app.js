ca17534a4f8ab51881d8f817558bb2bb89d67318
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-10-08 12:34:38 +0700

    updateeee

--- mdp-app/app.js
+++ mdp-app/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressLayout)
+app.use(expressLayout);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
