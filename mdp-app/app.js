2761235e3ecb34392925effb3a9f6710520077d7
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-10-19 22:42:03 +0700

    update api

--- mdp-app/app.js
+++ mdp-app/app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressLayout = require("express-ejs-layouts");
+// const cors = require("cors");
+const connectDB = require("./app_api/models/db");
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var prodiRouter = require('./app_server/routes/prodi')
+var prodiRouter = require('./app_server/routes/prodi');
+const fakultasRouter = require("./app_api/routes/fakultas");
 
 var app = express();
 
@@ -25,6 +28,10 @@ app.use(expressLayout);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/prodi', prodiRouter);
+app.use("/api/fakultas", fakultasRouter);
+
+// connect to mongoDB
+connectDB();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
