41f616ffd63eb11d97ad21f7cea63468946ddb4d
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-11-05 10:19:08 +0700

    puah

---
+++ app_server/controllers/fakultasController.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+const index = async (req, res) => {
+    try {
+        // mendapatkan data fakultas dari API eksternal
+        const response = await axios.get(
+            
+        )
+    }
+};
